9dcc21c1c4ea05579078ea688c03e0cc0a0fa961
dwddavis <50349984+dwddavis@users.noreply.github.com>
2020-07-21 22:50:37 -0500

    Update 5.Hangman.py
    7-21

--- 5.Hangman.py
+++ 5.Hangman.py
@@ -4,45 +4,44 @@ wordBank = ["messy room", "sleepy baby", "sandwich meat", "pepperoni pizza", "ju
             "pesto and pasta", "sweet and sour cause", "cream cheese bagel", "double cheeseburger", "candied apples",
             "italian sausage", "baby shark song"]
 
+
 def gamePlay():
-    gameWord = random.randint(0, len(wordBank)-1)
-    roundWord = (wordBank[int(gameWord)])
-    print(roundWord)
-    blankWord = []
+        gameWord = random.randint(0, len(wordBank) - 1)
+        roundWord = (wordBank[int(gameWord)])
+        print(roundWord)
+        blankWord = []
 
-    for i in roundWord:
-        if ord(i) > 96:
-            blankWord.append("_")
-        else:
-            blankWord.append(" ")
-
-    letterUsed=[]
-    cleanWord = ""
-    
-
-    def oneRound():
-        
-        tick = -1
-        count = -1
-        print("\n\n\n", cleanWord.join(blankWord))
-        print("Tries Left", tries)
-        print("\n\nLetters Used", letterUsed)
-
-        userGuess = input("Guess a Letter \n")
-        letterUsed.append(userGuess)
-
-        for j in roundWord:
-            tick = tick + 1
-
-            if userGuess == j:
-                blankWord[tick] = userGuess
+        for i in roundWord:
+            if ord(i) > 96:
+                blankWord.append("_")
             else:
-                count = count + 1
+                blankWord.append(" ")
 
-        if count == tick:
-            
+        letterUsed = []
+        cleanWord = ""
+
+
+        def oneRound():
+
+            tick = -1
+            count = -1
+            print("\n\n\n", cleanWord.join(blankWord))
+            print("Tries Left", "")
+            print("\n\nLetters Used", letterUsed)
+
+            userGuess = input("Guess a Letter \n")
+            letterUsed.append(userGuess)
+
+            for j in roundWord:
+                tick = tick + 1
+
+                if userGuess == j:
+                    blankWord[tick] = userGuess
+                else:
+                    count = count + 1
+
+            oneRound()
 
         oneRound()
 
-    oneRound()
 gamePlay()
